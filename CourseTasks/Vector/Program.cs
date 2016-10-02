719ea1067f43564f85d9829aae4612bb28311a05
lihopavlov <pnr@comfortlab.ru>
2016-10-02 18:01:06 +0600

    Некотрые изменения

--- CourseTasks/Vector/Program.cs
+++ CourseTasks/Vector/Program.cs
@@ -11,6 +11,7 @@ namespace Vector
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Hi, man!");
         }
     }
 }
