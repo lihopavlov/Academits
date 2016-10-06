63a27d2506fdb58d56b04d3dd0b887a5ea3c61ce
lihopavlov <pnr@comfortlab.ru>
2016-10-06 10:21:07 +0600

    Создан класс Shape. В Main проекта Vector создаются все объекты + дополнительные функции для работы с коллекцией.

--- CourseTasks/Vector/Program.cs
+++ CourseTasks/Vector/Program.cs
@@ -8,42 +8,60 @@ namespace Vector
 {
     class Program
     {
+
+        private static IShape FindMaxArea(List<IShape> shapes)
+        {
+            ShapeAreaComparer areaComparer = new ShapeAreaComparer();
+            shapes.Sort(areaComparer);
+            return shapes[shapes.Count - 1];
+        }
+
+        private static IShape FindNMaxPerimeter(List<IShape> shapes, int nIndex)
+        {
+            ShapePerimeterComparer perimeterComparer = new ShapePerimeterComparer();
+            shapes.Sort(perimeterComparer);
+
+            if (nIndex < 1 || nIndex > shapes.Count)
+            {
+                return null;
+            }
+            return shapes[shapes.Count - nIndex];
+        }
+
         static void Main(string[] args)
         {
-            Range range1 = new Range(3.0, 10.0);
-            Range range2 = new Range(1.0, 5.0);
-            Range range3 = new Range(8.0, 11.0);
-            Range range4 = new Range(4.0, 9.0);
-            Range range5 = new Range(0.5, 11.0);
-            Range range6 = new Range(11.0, 15.0);
-            Range range7 = new Range(10.0, 15.0);
-            Range range8 = new Range(2.0, 3.0);
-            Range range9 = new Range(3.0, 10.0);
+            const int secondMaxPerimeter = 2;
 
-            Range.ShowIntersection(range1, range2);
-            Range.ShowIntersection(range1, range3);
-            Range.ShowIntersection(range1, range4);
-            Range.ShowIntersection(range1, range5);
-            Range.ShowIntersection(range1, range6);
-            Console.WriteLine();
+            List<IShape> shapes = new List<IShape>();
+            shapes.Add(new Square(5.0));
+            shapes.Add(new Square(4.0));
+            shapes.Add(new Rectangle(2.0, 6.0));
+            shapes.Add(new Rectangle(3.0, 4.0));
+            shapes.Add(new Triangle(5.0, 1.0, 7.0, 4.0, 2.0, 5.0));
+            shapes.Add(new Triangle(5.0, 1.0, 8.0, 3.0, 2.5, 5.5));
+            shapes.Add(new Circle(5.0));
+            shapes.Add(new Circle(5.5));
 
-            Range.ShowUnion(range1, range2);
-            Range.ShowUnion(range1, range3);
-            Range.ShowUnion(range1, range4);
-            Range.ShowUnion(range1, range5);
-            Range.ShowUnion(range1, range6);
-            Range.ShowUnion(range1, range7);
-            Console.WriteLine();
+            ShapeAreaComparer areaComparer = new ShapeAreaComparer();
+            shapes.Sort(areaComparer);
+            Console.WriteLine("По возрастанию площади:");
+            foreach (IShape shape in shapes)
+            {
+                Console.WriteLine(shape);
+            }
 
-            Range.ShowDifference(range1, range2);
-            Range.ShowDifference(range1, range3);
-            Range.ShowDifference(range1, range4);
-            Range.ShowDifference(range1, range5);
-            Range.ShowDifference(range1, range6);
-            Range.ShowDifference(range1, range7);
-            Range.ShowDifference(range1, range8);
-            Range.ShowDifference(range1, range9);
-            Console.WriteLine();
+            ShapePerimeterComparer perimeterComparer = new ShapePerimeterComparer();
+            shapes.Sort(perimeterComparer);
+            Console.WriteLine("По возрастанию периметра:");
+            foreach (IShape shape in shapes)
+            {
+                Console.WriteLine(shape);
+            }
+
+            Console.WriteLine("Фигура с максимальной площадью ->");
+            Console.WriteLine(FindMaxArea(shapes));
+            Console.WriteLine("Фигура со вторым по величине периметром ->");
+            Console.WriteLine(FindNMaxPerimeter(shapes, secondMaxPerimeter));
 
         }
     }
